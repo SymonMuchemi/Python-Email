7592c991ae57148006d76684c99c19e17ba90940
symonmuchemi <simonkenya846@gmail.com>
2025-03-10 20:52:18 +0300

    refactor: improve code formatting and enhance readability in email worker

--- workers/shopsmart_email_worker.py
+++ workers/shopsmart_email_worker.py
@@ -1,3 +1,4 @@
+import time
 import redis
 import os
 import ssl, smtplib
@@ -8,54 +9,57 @@ from email.mime.multipart import MIMEMultipart
 # create redis client
 redis_client = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
-STREAM_NAME = 'shopsmart:mail'
-GROUP_NAME = 'shopsmart:mail_group'
-CONSUMER_NAME = 'alpha'
+STREAM_NAME = "shopsmart:mail"
+GROUP_NAME = "shopsmart:mail_group"
+CONSUMER_NAME = "alpha"
 
-load_dotenv('../config/config.env')
+load_dotenv("../config/config.env")
 
 # Create consumer group
 try:
-    redis_client.xgroup_create(STREAM_NAME, GROUP_NAME, id='0', mkstream=True)
+    redis_client.xgroup_create(STREAM_NAME, GROUP_NAME, id="0", mkstream=True)
 except Exception as e:
-    print(f'Error creating consumer group: {e}')
     pass
 
-SENDER_EMAIL = os.getenv('GMAIL_USERNAME')
-PASSWORD = os.getenv('GMAIL_PWD')
+SENDER_EMAIL = os.getenv("GMAIL_USERNAME")
+PASSWORD = os.getenv("GMAIL_PWD")
 PORT = 465
+
 
 def send_email(address, subject, body):
     msg = MIMEMultipart()
-    msg['from'] = SENDER_EMAIL
-    msg['to'] = address
-    msg['subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-    
+    msg["from"] = SENDER_EMAIL
+    msg["to"] = address
+    msg["subject"] = subject
+    msg.attach(MIMEText(body, "plain"))
+
     context = ssl.create_default_context()
-    
-    with smtplib.SMTP_SSL('smtp.gmail.com', port=PORT, context=context) as server:
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port=PORT, context=context) as server:
         server.login(SENDER_EMAIL, PASSWORD)
         server.sendmail(SENDER_EMAIL, address, msg.as_string())
-        
+
         # TODO: use logger instead
-        print('Email sent successfully')
+        print("Email sent successfully")
+
 
 while True:
     try:
         messages = redis_client.xreadgroup(
-            GROUP_NAME,
-            CONSUMER_NAME,
-            { STREAM_NAME: '>'},
-            count=1,
-            block=5000
+            GROUP_NAME, CONSUMER_NAME, {STREAM_NAME: ">"}, count=1, block=5000
         )
-        
+
+        if not messages:
+            print("waiting for messages...")
+            time.sleep(3)
+
         if messages:
             for stream, msg_list in messages:
+                print(f"Length of messages: {len(msg_list)}")
                 for msg_id, msg in msg_list:
-                    print(msg)
-                    # send_email(msg.email, msg.subject, msg.body)
+                    print(f'message: {msg_id}, email: {msg["email"]}')
+                    print(f"Type of message: {type(msg)}")
+                    send_email(msg["email"], msg["subject"], msg["body"])
                     redis_client.xack(STREAM_NAME, GROUP_NAME, msg_id)
     except Exception as e:
-        print(f'Error reading message: {e}')
+        print(f"Error reading message: {e}")
